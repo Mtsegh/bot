59e3c72daedebf20662eec658f71b01f06d49845
Phinehas Aondona <phinehasaondona007@gmail.com>
2024-09-16 23:06:10 +0100

    Docker

--- bot.js
+++ bot.js
@@ -1,19 +1,33 @@
 const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const {handle_message} = require('./messageFunctions/message');
 const handle_callback_data = require('./messageFunctions/callBackData');
 const { protect, deleteOldMessages } = require('./middleware/authMiddleware');
 
+const MONGO_URI="mongodb+srv://mtseghnadooihom:mtseghnadooihom@merchant.mz3erpr.mongodb.net/TelegramDataBot?retryWrites=true&w=majority&appName=Merchant"
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(MONGO_URI).then(() => {
     try {
         const bot = new TelegramBot('7485778029:AAHb3yymLQt1HSBllQcrCVrgUTqRuKU4iN8', { polling: true });
-        console.log("Bot is running...", process.env.TOKEN);
-        
+        const app = express();
+        app.use(bodyParser.json());
+        const PORT = process.env.PORT || 80;
+
+        app.get("/", (req, res) => {
+            res.send("Home Page")
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
         setInterval(() => {
             deleteOldMessages(bot);
         }, 24 * 60 * 60 * 1000); // Runs every day
+
         
         bot.on('callback_query', async (query) => {
             const chatId = query.message.chat.id;
